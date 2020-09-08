b4b6efa3220354407b8c6158e3403b89b8411c64
Manfred Touron <94029+moul@users.noreply.github.com>
2020-09-09 00:26:44 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+
+	"moul.io/motd"
+	"moul.io/srand"
+)
+
+func main() {
+	if err := run(os.Args); err != nil {
+		log.Fatalf("error: %v", err)
+		os.Exit(1)
+	}
+}
+
+// nolint:unparam
+func run(args []string) error {
+	rand.Seed(srand.Fast())
+	fmt.Print(motd.Default())
+	fmt.Println("Hello World!", args)
+	return nil
+}
